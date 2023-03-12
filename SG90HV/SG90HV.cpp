c8e908d0b519c3f76d2009ebe3d78a0aedfbe630
Marcus Wong <2799360989@qq.com>
2023-03-12 14:40:02 +0000

    TestSG90HV

--- SG90HV/SG90HV.cpp
+++ SG90HV/SG90HV.cpp
@@ -11,16 +11,17 @@ int main() {
     }
     // const int RANGE = 
 
-    int pin = 4;
+    int pin = 12;
 
     gpioSetMode(pin, PI_OUTPUT);
-    // gpioSetPWMfrequency(pin, 200);
-    gpioSetPWMrange(pin, 200); // 20000us = 20ms
-    int i = 0;
-    gpioPWM(pin, 100);
-    gpioSleep(PI_TIME_RELATIVE, 4, 0);
-    gpioPWM(pin, 200);
-    while(0) {
+    gpioSetPWMfrequency(pin, 50);
+    gpioSetPWMrange(pin, 100); // 20000us = 20ms
+    // int i = 0;
+    // gpioPWM(pin, 10);
+    for(int i = 48; i <= 120; ++i) {
+        cout << i*0.125 << endl;
+        gpioPWM(pin, i*0.125);
+        gpioSleep(PI_TIME_RELATIVE, 2, 0);
         // if (i == 0) {
         //     gpioPWM(pin, 20);
         //     // i++;
@@ -28,7 +29,7 @@ int main() {
         //     gpioPWM(pin, 0);
         // }
         // gpioPWM(pin, 50);
-        // gpioSleep(PI_TIME_RELATIVE, 4, 0);
+        // gpioSleep(PI_TIME_RELATIVE, 4gpioDelay(4000);, 0);
         // gpioPWM(pin, 50);
         // gpioSleep(PI_TIME_RELATIVE, 4, 0);
         // gpioDelay(4000);
@@ -39,7 +40,6 @@ int main() {
         // gpioPWM(pin, 20);
         // gpioSleep(PI_TIME_RELATIVE, 4, 0);
     }
-
     // terminate lib
     gpioTerminate();
 
